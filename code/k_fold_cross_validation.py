2eb989b63afc777ffbd8d5720077520306b0e9fb
Abdel <abdelkader.habel_pro@outlook.com>
2024-06-27 17:20:04 -0400

    added micro and macro measurements to kfold

--- code/k_fold_cross_validation.py
+++ code/k_fold_cross_validation.py
@@ -1,24 +1,29 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 from sklearn.model_selection import KFold
 from torch.utils.data import DataLoader, random_split
 from torchvision import datasets, transforms
 
+from Dataset import get_device
 from cnn_model import SimpleCNN
+
+# set random seed for reproducibility
+torch.manual_seed(20)
 
 transform = transforms.Compose([
     transforms.ToTensor()
 ])
 dataset = datasets.ImageFolder('data/normalized_data', transform=transform)
-
+device = get_device()
 kf = KFold(n_splits=10, shuffle=True, random_state=42)
 
 results = []
 
 # apply kfold cross validation
 for fold, (train_idx, test_idx) in enumerate(kf.split(dataset)):
+    print(f"Starting fold {fold + 1}...")
     # datasets
     train_subset = torch.utils.data.Subset(dataset, train_idx)
     test_subset = torch.utils.data.Subset(dataset, test_idx)
@@ -33,13 +38,20 @@ for fold, (train_idx, test_idx) in enumerate(kf.split(dataset)):
 
     # model, loss function and optimizer
     model = SimpleCNN()
+    model.to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
+    num_epochs = 20
+    patience = 12
     best_val_loss = float('inf')
-    for epoch in range(20):
+    patience_counter = 0
+
+    for epoch in range(num_epochs):
         model.train()
         for images, labels in train_loader:
+            images, labels = images.to(device), labels.to(device)
+
             optimizer.zero_grad()
             outputs = model(images)
             loss = criterion(outputs, labels)
@@ -47,9 +59,11 @@ for fold, (train_idx, test_idx) in enumerate(kf.split(dataset)):
             optimizer.step()
 
         model.eval()
-        val_loss = 0
+        val_loss = 0.0
         with torch.no_grad():
             for images, labels in val_loader:
+                images, labels = images.to(device), labels.to(device)
+
                 outputs = model(images)
                 loss = criterion(outputs, labels)
                 val_loss += loss.item()
@@ -58,7 +72,12 @@ for fold, (train_idx, test_idx) in enumerate(kf.split(dataset)):
 
         if val_loss < best_val_loss:
             best_val_loss = val_loss
+            patience_counter = 0
         else:
+            patience_counter += 1
+
+        if patience_counter >= patience:
+            print(f"Fold {fold + 1}: Early stopping, stopped at epoch: ", epoch + 1)
             break
 
     # evaluate on test set
@@ -66,34 +85,58 @@ for fold, (train_idx, test_idx) in enumerate(kf.split(dataset)):
     y_true, y_pred = [], []
     with torch.no_grad():
         for images, labels in test_loader:
+            images, labels = images.to(device), labels.to(device)
             outputs = model(images)
             _, preds = torch.max(outputs, 1)
             y_true.extend(labels.cpu().numpy())
             y_pred.extend(preds.cpu().numpy())
 
     accuracy = accuracy_score(y_true, y_pred)
-    precision = precision_score(y_true, y_pred, average='weighted', zero_division=1)
-    recall = recall_score(y_true, y_pred, average='weighted', zero_division=1)
-    f1 = f1_score(y_true, y_pred, average='weighted', zero_division=1)
+    macro_precision, macro_recall, macro_f1, _ = precision_recall_fscore_support(y_true, y_pred, average='macro',
+                                                                                 zero_division=1)
+    micro_precision, micro_recall, micro_f1, _ = precision_recall_fscore_support(y_true, y_pred, average='micro',
+                                                                                 zero_division=1)
 
     results.append({
         'fold': fold + 1,
         'accuracy': accuracy,
-        'precision': precision,
-        'recall': recall,
-        'f1_score': f1
+        'macro_precision': macro_precision,
+        'macro_recall': macro_recall,
+        'macro_f1_score': macro_f1,
+        'micro_precision': micro_precision,
+        'micro_recall': micro_recall,
+        'micro_f1_score': micro_f1
     })
 
 # calculate average results to see the 10-fold performance
 avg_accuracy = sum([result['accuracy'] for result in results]) / 10
-avg_precision = sum([result['precision'] for result in results]) / 10
-avg_recall = sum([result['recall'] for result in results]) / 10
-avg_f1 = sum([result['f1_score'] for result in results]) / 10
+avg_macro_precision = sum([result['macro_precision'] for result in results]) / 10
+avg_macro_recall = sum([result['macro_recall'] for result in results]) / 10
+avg_macro_f1 = sum([result['macro_f1_score'] for result in results]) / 10
 
-print("10-Fold Cross-Validation Results")
+avg_micro_precision = sum([result['micro_precision'] for result in results]) / 10
+avg_micro_recall = sum([result['micro_recall'] for result in results]) / 10
+avg_micro_f1 = sum([result['micro_f1_score'] for result in results]) / 10
+
+print("\n10-Fold Cross-Validation Results")
+print("---------------------------------------------")
 for result in results:
-    print(result)
-print(f"Average Accuracy: {avg_accuracy}")
-print(f"Average Precision: {avg_precision}")
-print(f"Average Recall: {avg_recall}")
-print(f"Average F1 Score: {avg_f1}")
+    print(f"Fold {result['fold']}: "
+          f"Accuracy: {result['accuracy']:.2f}, "
+          f"Macro Precision: {result['macro_precision']:.2f}, "
+          f"Macro Recall: {result['macro_recall']:.2f}, "
+          f"Macro F1 Score: {result['macro_f1_score']:.2f}, "
+          f"Micro Precision: {result['micro_precision']:.2f}, "
+          f"Micro Recall: {result['micro_recall']:.2f}, "
+          f"Micro F1 Score: {result['micro_f1_score']:.2f}")
+
+print("\nAverage Results")
+print("---------------------------------------------")
+print(f"Average Accuracy: {avg_accuracy:.2f}")
+print(f"Average Macro Precision: {avg_macro_precision:.2f}")
+print(f"Average Macro Recall: {avg_macro_recall:.2f}")
+print(f"Average Macro F1 Score: {avg_macro_f1:.2f}")
+
+print(f"Average Micro Precision: {avg_micro_precision:.2f}")
+print(f"Average Micro Recall: {avg_micro_recall:.2f}")
+print(f"Average Micro F1 Score: {avg_micro_f1:.2f}")
